3f8e7a3b28e48b977e5f99077fbd547b1ef99c5b
dev-hemul <dev.hemulll@gmail.com>
2024-06-20 18:23:08 +0300

    complete form

---
+++ script.js
@@ -0,0 +1,42 @@
+let search = document.querySelector(".form__input-search");
+let output = document.querySelector(".output");
+
+
+let arr = [
+	{id: 1, name: "Iphone", model: "X"},
+	{id: 2, name: "Iphone", model: "11"},
+	{id: 3, name: "Iphone", model: "12"},
+	{id: 4, name: "Iphone", model: "13"},
+	{id: 5, name: "Iphone", model: "14"},
+	{id: 6, name: "Iphone", model: "14"}
+]
+
+search.addEventListener("input", () => {
+	let inputSearch = search.value.toLowerCase().split(" ");
+	let searchName = inputSearch[0];
+	let searchModel = inputSearch[1];
+
+	if (search.value.trim() === '') {
+        output.textContent = "";
+        return;
+    }
+
+	let result = arr.filter(item =>
+		item.name.toLowerCase().includes(searchName) &&
+		item.model.toLowerCase().includes(searchModel)
+	);
+
+	if (result.length === 0) {
+		output.textContent = "Такого телефону не має в базі!"
+		output.style.color = "red";
+	}else {
+		output.textContent = "Такий телефон є в базі!"
+		output.style.color = "#008000";
+	}
+
+	console.log(result);
+})
+
+
+
+
